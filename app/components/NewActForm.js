aa2eb33ad18bb07902dffacb11f9f04c7c1e5535
Courtney Hoskins <courtneyhoskins@Courtneys-MBP.lan>
2023-06-10 19:50:12 -0700

    initial commit with functionality

---
+++ app/components/NewActForm.js
@@ -0,0 +1,35 @@
+import { useState, useRef } from 'react'
+
+export default function AddActForm({ onAdd }) {
+    const dataRef = useRef()
+    const [title, setTitle] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const submitAct = {
+          name: title
+        }
+        fetch(`http://localhost:8080/acts/`, {
+          method: 'POST',
+          body: JSON.stringify(submitAct),
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        })
+        onAdd()
+      }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          className='mr-5 text-slate-800'
+          type='text'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter Act Title"
+        />
+        <button type="submit">Add Act</button>
+      </form>
+    )
+  }
---
+++ app/components/NewBeatForm.js
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+export default function AddBeatForm(props) {
+    const [name, setTitle] = useState('')
+    const [content, setDesc] = useState('')
+    const [time, setTime] = useState('')
+    const [notes, setNotes] = useState('')
+    const [cameraAngle, setCamera] = useState('')
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const submitBeat = {
+        name,
+        time,
+        content,
+        cameraAngle,
+        notes
+      }
+      try {
+        await fetch(`http://localhost:8080/acts/${props.id}/beats`, {
+        method: 'POST',
+        body: JSON.stringify(submitBeat),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      })
+      props.onAdd()
+    } catch (error) {
+      console.error(error)
+    }
+    }
+    
+
+    return (
+      <form onSubmit={handleSubmit} className='container flex flex-col justify-start gap-5 basis-1/4 text-slate-800'>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder='Enter beat title'
+        />
+        <input
+          type='text'
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          placeholder='Enter beat timing or duration'
+        />
+        <input
+          type='text'
+          value={content}
+          onChange={(e) => setDesc(e.target.value)}
+          placeholder='Enter beat description'
+        />
+        <input
+          type='text'
+          value={cameraAngle}
+          onChange={(e) => setCamera(e.target.value)}
+          placeholder='Enter camera angle or shot'
+        />
+        <input
+          type='text'
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+          placeholder='Any additional notes?'
+        />
+
+        <button type='submit' className='bg-emerald-500 p-2 text-slate-100'>Add Beat</button>
+      </form>
+    )
+  }
